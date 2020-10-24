d068a5b535bb9048ea1e036d5f1a9dcb21b5e737
qusaieilouti99 <qusaieilouti@gmail.com>
2020-10-24 18:39:16 +0300

    first task

--- lib/main.dart
+++ lib/main.dart
@@ -1,98 +1,125 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'myState.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  FocusNode focusNode = FocusNode();
+  bool valid = true;
+  bool require = true;
+  bool isfocused = false;
+  bool isempty = true;
+  String myValue;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    focusNode.addListener(() {
+      focusNode.hasFocus ? isfocused = true : isfocused = false;
+    });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    focusNode.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (BuildContext context) => TextFieldState(),
-      child: MaterialApp(
-        home: Scaffold(
-          body: Consumer<TextFieldState>(
-            builder: (BuildContext context, myState, Widget child) {
-              return Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          width: myState.isfocused ? 4 : 1,
-                          color: myState.valid ? Colors.black : Colors.red,
+    return MaterialApp(
+      home: Scaffold(
+          body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  width: isfocused ? 4 : 1,
+                  color: valid ? Colors.black : Colors.red,
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 10, right: 10),
+                child: TextFormField(
+                  focusNode: focusNode,
+                  textAlignVertical: TextAlignVertical(y: -1),
+                  onEditingComplete: () {
+                    setState(() {
+                      setState(() {
+                        isfocused = false;
+                      });
+                    });
+                  },
+                  onTap: () {
+                    setState(() {
+                      focusNode.hasFocus ? isfocused = true : isfocused = false;
+                    });
+                  },
+                  onChanged: (value) {
+                    setState(() {
+                      focusNode.hasFocus ? isfocused = true : isfocused = false;
+                      isempty = false;
+                      require = false;
+                      myValue = value;
+                    });
+                  },
+                  textInputAction: TextInputAction.done,
+                  style: valid
+                      ? TextStyle(color: Colors.black, fontSize: 20)
+                      : TextStyle(
+                          color: Colors.red,
+                          fontSize: 20,
                         ),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 10, right: 10),
-                        child: TextFormField(
-                          focusNode: myState.focusNode,
-                          textAlignVertical: TextAlignVertical(y: -1),
-                          onTap: () {
-                            myState.onTap();
-                          },
-                          onChanged: (value) {
-                            myState.onChanged(value);
-                          },
-                          textInputAction: TextInputAction.done,
-                          style: myState.valid
-                              ? TextStyle(color: Colors.black, fontSize: 20)
-                              : TextStyle(
-                                  color: Colors.red,
-                                  fontSize: 20,
-                                ),
-                          decoration: InputDecoration(
-                            alignLabelWithHint: true,
-                            labelText: myState.isempty && !myState.isfocused
-                                ? null
-                                : "Email",
-                            floatingLabelBehavior: FloatingLabelBehavior.auto,
-                            labelStyle: TextStyle(
-                                color: myState.valid && myState.isfocused
-                                    ? Colors.amber
-                                    : Colors.grey,
-                                fontSize: 20),
-                            suffixIcon: myState.isempty
-                                ? Text('*',
-                                    style: TextStyle(
-                                        color: Colors.amber, fontSize: 20))
-                                : null,
-                            contentPadding:
-                                myState.isempty ? EdgeInsets.all(20) : null,
-                            suffixIconConstraints: BoxConstraints(
-                                maxHeight: 20,
-                                maxWidth: 20,
-                                minHeight: 20,
-                                minWidth: 20),
-                            //errorText: valid ? null : null,
-                            hintText: 'Enter Email ',
-                            hintStyle: TextStyle(fontSize: 20),
-                            // border: UnderlineInputBorder(),
-                            focusedBorder: UnderlineInputBorder(
-                                borderSide: BorderSide(width: 0)),
-                          ),
-                        ),
-                      ),
-                    ),
+                  decoration: InputDecoration(
+                    alignLabelWithHint: true,
+                    labelText: isempty && !isfocused ? null : "Email",
+                    floatingLabelBehavior: FloatingLabelBehavior.auto,
+                    labelStyle: TextStyle(
+                        color: valid && isfocused ? Colors.amber : Colors.grey,
+                        fontSize: 20),
+                    suffixIcon: isempty
+                        ? Text('*',
+                            style: TextStyle(color: Colors.amber, fontSize: 20))
+                        : null,
+                    contentPadding: !isfocused ? EdgeInsets.all(20) : null,
+                    suffixIconConstraints: BoxConstraints(
+                        maxHeight: 20,
+                        maxWidth: 20,
+                        minHeight: 20,
+                        minWidth: 20),
+                    //errorText: valid ? null : null,
+                    hintText: 'Enter Email ',
+                    hintStyle: TextStyle(fontSize: 20),
+                    // border: UnderlineInputBorder(),
+                    focusedBorder:
+                        UnderlineInputBorder(borderSide: BorderSide(width: 0)),
                   ),
-                  RaisedButton(
-                    onPressed: () {
-                      myState.buttonOnPressed();
-                    },
-                    child: Text('Submit'),
-                    textColor: Colors.white,
-                    color: Colors.brown,
-                  ),
-                ],
-              );
-            },
+                ),
+              ),
+            ),
           ),
-        ),
-      ),
+          RaisedButton(
+            onPressed: () {
+              setState(() {
+                myValue.contains('@') ? valid = true : valid = false;
+              });
+            },
+            child: Text('Submit'),
+            textColor: Colors.white,
+            color: Colors.brown,
+          ),
+        ],
+      )),
     );
   }
 }
